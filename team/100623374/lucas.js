afa2e1f42950135cc659e020f9ea3175a4094a36
chopsta-dex <lucas.b@outlook.com.au>
2023-11-13 11:48:13 +1100

    Create lucas.js

---
+++ team/100623374/lucas.js
@@ -0,0 +1,4 @@
+// File Name: lucas.js
+// Description: Should end up containing the code to do the flippies on the card
+// with the image on the back of it. On the front of the card should be my name
+// and student ID.
